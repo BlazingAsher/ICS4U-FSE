94b2899d068de0941023f70605d5edeb2545499f
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2020-05-10 10:10:21 -0400

    forgot

---
+++ Source/ICS4UFSE_CPP/Skulkin.cpp
@@ -0,0 +1,37 @@
+// Copyright (c) 2020 David Hui and Kevin Zhang. ALL RIGHTS RESERVED.
+
+#include<cmath>
+#include "Skulkin.h"
+#include "Engine/World.h"
+
+// Sets default values
+ASkulkin::ASkulkin()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASkulkin::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASkulkin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector PlayerPos = GetWorld()->GetFirstPlayerController()->GetPawn()->GetActorLocation(), ThisPos = GetActorLocation();
+	FVector PosDiff = PlayerPos - ThisPos;
+
+	FRotator ThisRotation = { std::fmod(std::atan2(PosDiff.X, PosDiff.Z) * 180.0f / 3.1415926535897932f + 360.0f, 360.0f), std::fmod(std::atan2(PosDiff.Y, PosDiff.X) * 180.0f / 3.1415926535897932f + 360.0f, 360.0f), 0.0f };
+	SetActorRotation(ThisRotation);
+	SetActorLocation(ThisPos);
+	//ThisRotation.X, ThisRotation.Y, ThisRotation.Z
+	//UE_LOG(LogTemp, Warning, TEXT("%f, %f, %f\n"), ThisRotation.Roll, ThisRotation.Pitch, ThisRotation.Yaw);
+
+}
+
---
+++ Source/ICS4UFSE_CPP/Skulkin.h
@@ -0,0 +1,26 @@
+// Copyright (c) 2020 David Hui and Kevin Zhang. ALL RIGHTS RESERVED.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Skulkin.generated.h"
+
+UCLASS()
+class ICS4UFSE_CPP_API ASkulkin : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASkulkin();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
