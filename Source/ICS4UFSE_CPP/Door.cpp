fd44c10f9a5269e1b3273ae2d79623367104b2b5
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2020-05-21 22:55:29 -0400

    Built a house.

---
+++ Source/ICS4UFSE_CPP/Door.cpp
@@ -0,0 +1,113 @@
+// Copyright (c) 2020 David Hui and Kevin Zhang. ALL RIGHTS RESERVED.
+
+#define FinishAnimation()do{DegsAnimated=0;IsAnimating=0;}while(0)
+#include "Door.h"
+
+// Sets default values
+ADoor::ADoor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Temporary pivot for the door
+	Pivot = GetActorLocation() - FVector{ 0.5f, 0.0f, 0.0f };
+	// Set default degrees animated
+	DegsAnimated = 0;
+
+}
+
+// Called when the game starts or when spawned
+void ADoor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADoor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (IsAnimating)
+	{
+		// test if animation should be finished
+		if (DegsAnimated < 90)
+		{
+			// gets the location and rotation
+			FRotator ThisRot = GetActorRotation();
+			FVector ThisPos = GetActorLocation();
+			FRotator PivDiff = (ThisPos - Pivot).ToOrientationRotator();
+
+			if (YawPos == IsOpen)
+				ThisRot.Yaw += 3;
+			else
+				ThisRot.Yaw -= 3;
+			if (YawPos == IsOpen)
+				PivDiff.Yaw += 3;
+			else
+				PivDiff.Yaw -= 3;
+			ThisPos = Pivot + PivDiff.Vector() * (ThisPos - Pivot).Size();
+
+			SetActorRotation(ThisRot);
+			SetActorLocation(ThisPos);
+			DegsAnimated += 3;
+		}
+		else
+			FinishAnimation();
+	}
+
+}
+
+void ADoor::ToggleDoor()
+{
+	if (!IsAnimating)
+	{
+
+		// sets the state of the door
+		IsOpen = !IsOpen;
+		IsAnimating = true;
+
+	}
+}
+
+// Getters and setters
+
+bool ADoor::Opened()
+{
+	return IsOpen;
+}
+
+void ADoor::Opened(bool b)
+{
+	IsOpen = b;
+}
+
+bool ADoor::Animating()
+{
+	return IsAnimating;
+}
+
+void ADoor::Animating(bool b)
+{
+	IsAnimating = b;
+}
+
+float ADoor::AnimDegrees()
+{
+	return DegsAnimated;
+}
+
+void ADoor::AnimDegrees(float f)
+{
+	DegsAnimated = f;
+}
+
+FVector ADoor::DoorPivot()
+{
+	return Pivot;
+}
+
+void ADoor::DoorPivot(const FVector& piv)
+{
+	Pivot = piv;
+}
---
+++ Source/ICS4UFSE_CPP/Door.h
@@ -0,0 +1,64 @@
+// Copyright (c) 2020 David Hui and Kevin Zhang. ALL RIGHTS RESERVED.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+//#include <string>
+#include "Door.generated.h"
+
+UCLASS()
+class ICS4UFSE_CPP_API ADoor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ADoor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Opens or closes the door
+	void ToggleDoor();
+
+	// Getters and setters
+	bool Opened();
+	void Opened(bool b);
+
+	bool Animating();
+	void Animating(bool b);
+
+	/*std::string OpenDirection();
+	void OpenDirection(const std::string& dir);
+	void OpenDirection(std::string&& dir);*/
+
+	float AnimDegrees();
+	void AnimDegrees(float f);
+
+	FVector DoorPivot();
+	void DoorPivot(const FVector&piv);
+
+protected:
+	// Is the door already open
+	bool IsOpen;
+	
+	// Is the door being opened or closed
+	bool IsAnimating;
+
+	// Direction for opening the door
+	bool YawPos;
+
+	// How many degrees the door turned
+	float DegsAnimated;
+
+	UPROPERTY(EditDefaultsOnly)
+	// The point where the door is turned about
+	FVector Pivot;
+
+};
